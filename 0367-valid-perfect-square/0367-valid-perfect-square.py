203ade543155768730133ef64e320ffeb1b34ceb
Adeeba Zaidi <adeebafatima2703@gmail.com>
2026-07-09 16:44:03 +0530

    Time: 10 ms (6.12%), Space: 12.4 MB (17.62%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        # for i in range (num+1):
+        #     if i*i == num:
+        #         return True
+        # return False
+        i=0
+        while i*i<=num:
+            if i*i==num:
+                return True
+            i+=1
+        return False
